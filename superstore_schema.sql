bffc3abbde99faffb86d0a8e31d2968a4785413a
Mendelsohn Chan <chan.mendelsohn.neil@gmail.com>
2018-01-29 22:52:40 -0500

    Add files via upload

---
+++ superstore_schema.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE IF NOT EXISTS superstore_schema;
+
+USE superstore_schema;
+
+DROP TABLE IF EXISTS Orders; 
+
+CREATE TABLE IF NOT EXISTS Accommodation
+(
+  row_id varchar(255),
+  order_id varchar(255), 
+  order_date date, 
+  ship_date date, 
+  ship_mode varchar(255),
+  customer_id varchar(255), 
+  customer_name varchar(255),
+  segment varchar(100),
+  country varchar(100),
+  city varchar(100),
+  state varchar(100),
+  postal_code varchar(100),
+  region varchar(100),
+  product_id varchar(100), 
+  category varchar(100),
+  sub_category varchar(100),
+  product_name varchar(100),
+  sales int, 
+  quantity int, 
+  discount float, 
+  profit int, 
+  PRIMARY KEY (order_id)
+);
